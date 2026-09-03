24afb6587e29bdf3612760b9f83414d937c7e3a0
kolosovpetro <kolosovp94@gmail.com>
2026-09-03 14:53:32 +0200

    updated

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -1,11 +1,16 @@
 output "principal_id" {
-  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  value = azurerm_kubernetes_cluster.this.identity[0].principal_id
 }
 
 output "name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  value = azurerm_kubernetes_cluster.this.name
 }
 
 output "resource_group_name" {
-  value = azurerm_kubernetes_cluster.aks.resource_group_name
+  value = azurerm_kubernetes_cluster.this.resource_group_name
 }
+
+output "connect_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_kubernetes_cluster.this.resource_group_name} --name ${azurerm_kubernetes_cluster.this.name} --subscription ${data.azurerm_client_config.current.subscription_id} --overwrite-existing"
+}
+
--- output.tf
+++ output.tf
@@ -11,7 +11,7 @@ output "subscription" {
 }
 
 output "connect_command" {
-  value = "az aks get-credentials --resource-group ${azurerm_resource_group.public.name} --name ${module.aks.name} --subscription ${data.azurerm_client_config.current.subscription_id} --overwrite-existing"
+  value = module.aks.connect_command
 }
 
 output "grafana_endpoint" {
